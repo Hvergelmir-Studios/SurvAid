0b21e32a1aaf26e22d5d62ad37da491133e3dbad
Christian Allen <chin.allen9@hotmail.com>
2021-10-21 15:14:17 -0600

    Added Question, MultipleChoice, TextAnswer to quiz.py

--- SurvAidServer/api/models/quiz.py
+++ SurvAidServer/api/models/quiz.py
@@ -7,6 +7,19 @@ class Quiz(models.Model):
     title = models.CharField(max-length=128)
     created_by = models.ForeignKey('Instructor', on_delete=models.CASCADE)
     course = models.ForeignKey('Course', on_delete=modles.CASCADE)
-    date_created = models.DateField()
-    questions = models.ManyToManyField('Question')
+    date_created = models.DateField(auto_now_add=True)
     finished_by = models.ManyToManyField('Student')
+
+    def __str__(self):
+        return self.title
+
+class Question(models.Model):
+    quiz = models.ForeignKey('Quiz', on_delete=models.CASCADE)
+    type = models.CharField(max-length=64)
+    description = models.TextField()
+
+class MultipleChoice(Question):
+    pass
+
+class TextAnswer(Question):
+    pass
