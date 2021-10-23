29d36638939caea1de48c02da865ed5b85c54655
Christian Allen <chin.allen9@hotmail.com>
2021-10-22 22:35:46 -0600

    Fixed syntax errors

--- SurvAidServer/api/models/course.py
+++ SurvAidServer/api/models/course.py
@@ -2,6 +2,6 @@ from django.db import models
 from django.conf import settings
 
 class Course(models.Model):
-    name = models.CharField(max-length=64)
-    course_id = models.CharField(max-length=16)
-    created_by = models.ForeignKey('Instructor', on-on_delete=models.CASCADE)
+    name = models.CharField(max_length=64)
+    course_id = models.CharField(max_length=16)
+    created_by = models.ForeignKey('Instructor', on_delete=models.CASCADE)
--- SurvAidServer/api/models/quiz.py
+++ SurvAidServer/api/models/quiz.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.conf import settings
 
 class Quiz(models.Model):
-    title = models.CharField(max-length=128)
+    title = models.CharField(max_length=128)
     created_by = models.ForeignKey('Instructor', on_delete=models.CASCADE)
     course = models.ForeignKey('Course', on_delete=models.CASCADE)
     date_created = models.DateField(auto_now_add=True)
@@ -14,14 +14,14 @@ class Quiz(models.Model):
 
 class Question(models.Model):
     quiz = models.ForeignKey('Quiz', on_delete=models.CASCADE)
-    type = models.CharField(max-length=64)
+    type = models.CharField(max_length=64)
     description = models.TextField()
 
     def __str__(self):
         return f'{self.type}: {self.description}'
 
 class Choice(models.Model):
-    choice = models.CharField(max-length=256)
+    choice = models.CharField(max_length=256)
 
     def __str__(self):
         return self.choice
