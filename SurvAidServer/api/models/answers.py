c3f6b7a1cfc9afb241733e4edbe8886669ee4a69
Christian Allen <chin.allen9@hotmail.com>
2021-10-24 22:27:16 -0600

    Started users.py, completed answers.py

--- SurvAidServer/api/models/answers.py
+++ SurvAidServer/api/models/answers.py
@@ -1,4 +1,9 @@
 from django.db import models
 from django.conf import settings
 
-# from api.models import Quiz, Student
+class Answer(models.Model):
+    question = models.ForeignKey('Question', on_delete=CASCADE)
+    answer = models.TextField()
+    student = models.ForeignKey('Student', on_delete=CASCADE)
+    answered_on = models.DateField(auto_now_add=True)
+    is_correct = BooleanField(default=False)
--- SurvAidServer/api/models/users.py
+++ SurvAidServer/api/models/users.py
@@ -1,4 +1,12 @@
 from django.db import models
 from django.conf import settings
+from django.contrib.auth.models import AbstractUser
 
-# from api.models import Course 
+class User(AbstractUser):
+    
+
+class Instructor(User):
+    pass
+
+class Student(User):
+    pass
